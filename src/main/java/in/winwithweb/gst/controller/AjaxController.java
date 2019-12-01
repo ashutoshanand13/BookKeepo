05d08282c2d7545134a37a73751e84352ffb2558
sachingoyal <sachingoyal@192.168.1.104>
2019-12-01 13:22:20 +0530

    Refactoring the code

---
+++ src/main/java/in/winwithweb/gst/controller/AjaxController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package in.winwithweb.gst.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import in.winwithweb.gst.service.AccountService;
+
+/**
+ * @author sachingoyal
+ *
+ */
+
+@Controller
+public class AjaxController {
+	
+	@Autowired
+	private AccountService accountService;
+	
+	@RequestMapping(value = "/home/getGstinData", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody String getSuperVisiorData(@RequestParam String accountNbr) {
+		return new Gson().toJson(accountService.findAccountByAccountName(accountNbr));
+	}
+
+}
--- src/main/java/in/winwithweb/gst/controller/PaymentController.java
+++ src/main/java/in/winwithweb/gst/controller/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import in.winwithweb.gst.model.Receipts;
+import in.winwithweb.gst.model.Payment;
 import in.winwithweb.gst.service.AccountService;
 
 /**
@@ -18,26 +18,19 @@ import in.winwithweb.gst.service.AccountService;
  */
 
 @Controller
-public class HomeController {
-
+public class PaymentController {
+	
 	@Autowired
 	private AccountService accountService;
-
-	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public ModelAndView getAddAccount() {
+	
+	@RequestMapping(value = { "/home/addpayment" }, method = RequestMethod.GET)
+	public ModelAndView addPayment() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("home");
-		return modelAndView;
-	}
-
-	@RequestMapping(value = { "/addReceipt" }, method = RequestMethod.GET)
-	public ModelAndView getAddReceipt() {
-		ModelAndView modelAndView = new ModelAndView();
-		Receipts receipt = new Receipts();
-		modelAndView.addObject("receipts", receipt);
-		modelAndView.setViewName("addReceipt");
+		Payment payment = new Payment();
+		modelAndView.addObject("payment", payment);
+		modelAndView.setViewName("addPayment");
 		modelAndView.addObject("accountList", accountService.fetchAccountName());
 		return modelAndView;
-	}
+	}	
 
 }
