941ddef9bb33e6d94ccac4e12a327e950cb8d4e1
ashut <ashut@DESKTOP-G2DLFKN>
2019-12-07 15:58:06 +0530

    Sync

--- src/main/java/in/winwithweb/gst/controller/HomeController.java
+++ src/main/java/in/winwithweb/gst/controller/HomeController.java
@@ -3,24 +3,10 @@
  */
 package in.winwithweb.gst.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.google.gson.Gson;
-
-import in.winwithweb.gst.model.Accounts;
-import in.winwithweb.gst.model.Payment;
-import in.winwithweb.gst.model.Receipts;
-import in.winwithweb.gst.service.AccountService;
 
 /**
  * @author sachingoyal
@@ -30,82 +16,14 @@ import in.winwithweb.gst.service.AccountService;
 @Controller
 public class HomeController {
 
-	@Autowired
-	private AccountService accountService;
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public ModelAndView getHomePage() {
-		ModelAndView modelAndView = new ModelAndView();
-		Accounts account = new Accounts();
-		modelAndView.addObject("account", account);
-		modelAndView.setViewName("addaccount");
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/home", method = RequestMethod.POST)
-	public ModelAndView addNewAccount(@Valid @ModelAttribute("account") Accounts account, BindingResult bindingResult) {
-		ModelAndView modelAndView = new ModelAndView();
-		Accounts accountWithGstInExists = accountService.findAccountByGstin(account.getGstin());
-
-		if (accountWithGstInExists != null) {
-			bindingResult.rejectValue("gstin", "gstin", "This GST number is already registered.");
-		}else if(account.getGstin().trim().length() != 15) {
-			bindingResult.rejectValue("gstin", "gstin", "Please provide a valid GST number.");
-		}
-
-		Accounts accountWithPanExists = accountService.findAccountByPan(account.getAccountPan());
-		if (accountWithPanExists != null) {
-			bindingResult.rejectValue("accountPan", "accountPan", "This PAN is already registered.");
-		}else if(account.getAccountPan().trim().length() != 10) {
-			bindingResult.rejectValue("accountPan", "accountPan", "Please provide a valid PAN.");
-
-		}
-
-		if (bindingResult.hasErrors()) {
-			modelAndView.addObject("account", account);
-			modelAndView.setViewName("addaccount");
-		} else {
-			accountService.saveAccount(account);
-			modelAndView.addObject("message", "New Account Details Successfully Added");
-			modelAndView.addObject("account", new Accounts());
-			modelAndView.setViewName("addaccount");
-		}
-		return modelAndView;
-	}
-	
-	
-	@RequestMapping(value = { "/addPayment" }, method = RequestMethod.GET)
-	public ModelAndView addPayment() {
-		ModelAndView modelAndView = new ModelAndView();
-		Payment payment = new Payment();
-		modelAndView.addObject("payment", payment);
-		modelAndView.setViewName("addPayment");
-		modelAndView.addObject("accountList", accountService.fetchAccountName());
-		return modelAndView;
-	}
-	
-	@RequestMapping(value = "/getGstinData", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody String getSuperVisiorData(@RequestParam String accountNbr) {
-		return new Gson().toJson(accountService.findAccountByAccountName(accountNbr));
-	}
-	
-	@RequestMapping(value = { "/addaccount" }, method = RequestMethod.GET)
 	public ModelAndView getAddAccount() {
 		ModelAndView modelAndView = new ModelAndView();
-		Accounts account = new Accounts();
-		modelAndView.addObject("account", account);
-		modelAndView.setViewName("addaccount");
+		modelAndView.setViewName("home");
 		return modelAndView;
 	}
+
 	
-	@RequestMapping(value = { "/addReceipt" }, method = RequestMethod.GET)
-	public ModelAndView getAddReceipt() {
-		ModelAndView modelAndView = new ModelAndView();
-		Receipts receipt = new Receipts();
-		modelAndView.addObject("receipts", receipt);
-		modelAndView.setViewName("addReceipt");
-		modelAndView.addObject("accountList", accountService.fetchAccountName());
-		return modelAndView;
-	}
-	
+
 }
