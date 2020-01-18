62a33340ebe58a7af00af28cc50db78b1f4312cd
ashut <ashut@192.168.0.27>
2020-01-18 16:08:15 +0530

    Add Company Profile Changes

---
+++ src/main/java/in/winwithweb/gst/controller/CompanyController.java
@@ -0,0 +1,34 @@
+package in.winwithweb.gst.controller;
+
+import java.security.Principal;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import in.winwithweb.gst.model.Accounts;
+import in.winwithweb.gst.model.Company;
+
+@Controller
+public class CompanyController {
+	
+	@RequestMapping(value = { "/home/addcompany" }, method = RequestMethod.GET)
+	public ModelAndView getAddCompanyPage() {
+		ModelAndView modelAndView = new ModelAndView();
+		Company company = new Company();
+		modelAndView.addObject("company", company);
+		modelAndView.setViewName("addCompany");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/home/company", method = RequestMethod.POST)
+	public String addCompany(@Valid @ModelAttribute("Company") Accounts account, BindingResult bindingResult, Principal principal) {
+		return "addCompany";
+	}
+	
+}
---
+++ src/main/java/in/winwithweb/gst/model/Company.java
@@ -0,0 +1,86 @@
+package in.winwithweb.gst.model;
+
+public class Company {
+	
+	private String companyName;
+	private String companyGstin;
+	private String companyState;
+	private String companyAddress;
+	private String companyEmail;
+	private String companyTelephone;
+	/**
+	 * @return the companyName
+	 */
+	public String getCompanyName() {
+		return companyName;
+	}
+	/**
+	 * @param companyName the companyName to set
+	 */
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	/**
+	 * @return the companyGstin
+	 */
+	public String getCompanyGstin() {
+		return companyGstin;
+	}
+	/**
+	 * @param companyGstin the companyGstin to set
+	 */
+	public void setCompanyGstin(String companyGstin) {
+		this.companyGstin = companyGstin;
+	}
+	/**
+	 * @return the companyState
+	 */
+	public String getCompanyState() {
+		return companyState;
+	}
+	/**
+	 * @param companyState the companyState to set
+	 */
+	public void setCompanyState(String companyState) {
+		this.companyState = companyState;
+	}
+	/**
+	 * @return the companyAddress
+	 */
+	public String getCompanyAddress() {
+		return companyAddress;
+	}
+	/**
+	 * @param companyAddress the companyAddress to set
+	 */
+	public void setCompanyAddress(String companyAddress) {
+		this.companyAddress = companyAddress;
+	}
+	/**
+	 * @return the companyEmail
+	 */
+	public String getCompanyEmail() {
+		return companyEmail;
+	}
+	/**
+	 * @param companyEmail the companyEmail to set
+	 */
+	public void setCompanyEmail(String companyEmail) {
+		this.companyEmail = companyEmail;
+	}
+	/**
+	 * @return the companyTelephone
+	 */
+	public String getCompanyTelephone() {
+		return companyTelephone;
+	}
+	/**
+	 * @param companyTelephone the companyTelephone to set
+	 */
+	public void setCompanyTelephone(String companyTelephone) {
+		this.companyTelephone = companyTelephone;
+	}
+	
+	
+
+}
