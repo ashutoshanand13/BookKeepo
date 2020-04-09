80934f0ff26bcacff13f2322573c31c9296e8eb3
sachingoyal <sachingoyal@192.168.1.103>
2020-04-09 18:46:31 +0530

    Changes for reports

--- src/main/java/in/winwithweb/gst/controller/ItemController.java
+++ src/main/java/in/winwithweb/gst/controller/ItemController.java
@@ -26,29 +26,29 @@ import in.winwithweb.gst.service.ItemService;
 
 @Controller
 public class ItemController {
-	
+
 	@Autowired
 	private ItemService itemService;
 
 	@RequestMapping(value = { "/home/additem" }, method = RequestMethod.GET)
 	public ModelAndView getItemPage(HttpServletRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
-		String user=request.getUserPrincipal().getName();
+		String user = request.getUserPrincipal().getName();
 		modelAndView.addObject("itemList", itemService.findByProductOwner(user));
 		modelAndView.addObject("item", new InvoiceProductDetails());
-		
+
 		modelAndView.setViewName("addItem");
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(value = "/home/additem", method = RequestMethod.POST)
 	public ModelAndView addItem(@Valid @ModelAttribute("item") InvoiceProductDetails item, BindingResult bindingResult,
 			Principal principal) {
 		ModelAndView modelAndView = new ModelAndView();
-		String user=principal.getName();
+		String user = principal.getName();
 		InvoiceProductDetails isItemExists = itemService.findByProductDescription(item.getProductDescription(), user);
-		
-		if(isItemExists != null) {
+
+		if (isItemExists != null) {
 			isItemExists.setProductDescription(item.getProductDescription());
 			isItemExists.setProductHnscode(item.getProductHnscode());
 			isItemExists.setProductUom(item.getProductUom());
@@ -59,8 +59,7 @@ public class ItemController {
 			itemService.saveItem(isItemExists);
 			modelAndView.addObject("message", "Item Updated Successfully");
 			modelAndView.addObject("item", item);
-		} 
-		else {
+		} else {
 			item.setProductOwner(user);
 			itemService.saveItem(item);
 			modelAndView.addObject("message", "Item Added Successfully");
@@ -70,5 +69,14 @@ public class ItemController {
 		modelAndView.setViewName("addItem");
 		return modelAndView;
 	}
-	
+
+	@RequestMapping(value = { "/home/showitem" }, method = RequestMethod.GET)
+	public ModelAndView showItem(Principal principal) {
+		ModelAndView modelAndView = new ModelAndView();
+		String user = principal.getName();
+		modelAndView.addObject("itemList", itemService.fetchAllItems(user));
+		modelAndView.setViewName("itemData");
+		return modelAndView;
+	}
+
 }
