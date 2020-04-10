b349f1186e1eadb4c9b7395f9cb93fbffbe9f0de
Yash Singh <ys4002@gmail.com>
2020-04-10 19:59:03 +0530

    add new item fix

--- src/main/java/in/winwithweb/gst/service/ItemService.java
+++ src/main/java/in/winwithweb/gst/service/ItemService.java
@@ -29,7 +29,7 @@ public class ItemService {
 
 	public List<String> findByProductOwner(String owner) {
 		ArrayList<String> itemList = new ArrayList<>();
-		itemList.add("");
+		itemList.add("Add New Item");
 		itemList.addAll(itemRepository.findByProductOwner(owner).stream().map(p -> p.getProductDescription())
 				.collect(Collectors.toList()));
 		return itemList;
