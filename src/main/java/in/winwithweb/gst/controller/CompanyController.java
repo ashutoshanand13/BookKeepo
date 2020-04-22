0c247fe6e5366540acfcc6156d419ce610b5ff7c
sachingoyal <sachingoyal@192.168.1.100>
2020-04-22 17:45:38 +0530

    Refactored Add company page

--- src/main/java/in/winwithweb/gst/controller/CompanyController.java
+++ src/main/java/in/winwithweb/gst/controller/CompanyController.java
@@ -29,72 +29,39 @@ public class CompanyController {
 	@RequestMapping(value = { "/home/addcompany" }, method = RequestMethod.GET)
 	public ModelAndView getAddCompanyPage(HttpServletRequest request) {
 		String user = request.getUserPrincipal().getName();
-		ModelAndView modelAndView = new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView("addCompany");
 		Company company = companyDetailsService.findByUserName(user);
-		if (company != null) {
-			modelAndView.addObject("company", company);
-			modelAndView.addObject("logoImage", CommonUtils.getImgfromByteArray(company.getCompanyLogo()));
+		if (company == null) {
+			company = new Company(user, CommonUtils.getImgfromResource("/static/images/image-400x400.jpg"));
 		} else {
-			modelAndView.addObject("company", new Company());
-			modelAndView.addObject("logoImage", CommonUtils.getImgfromResource("/static/images/image-400x400.jpg"));
+			company.setCompanyStringLogo(CommonUtils.getImgfromByteArray(company.getCompanyLogo()));
 		}
-
-		modelAndView.setViewName("addCompany");
+		modelAndView.addObject("company", company);
+		modelAndView.addObject("logoImage", company.getCompanyStringLogo());
 		return modelAndView;
 	}
 
 	@RequestMapping(value = "/home/addcompany", method = RequestMethod.POST)
 	public ModelAndView addCompany(@Valid @ModelAttribute("company") Company company, BindingResult bindingResult,
 			Principal principal, @RequestParam("companyLogo") MultipartFile companyLogo) {
-		ModelAndView modelAndView = new ModelAndView();
-		byte[] CompanyUploadedFile = null;
-		company.setUserName(principal.getName());
-		modelAndView.setViewName("addCompany");
-
-		Company isDataExists = companyDetailsService.findByUserName(principal.getName());
-
-		if (isDataExists == null) {
-			try {
-				CompanyUploadedFile = companyLogo.getBytes();
+		ModelAndView modelAndView = new ModelAndView(
+				CommonUtils.isPopulated(company.getPageName()) ? "redirect:" + company.getPageName() : "addCompany");
+		try {
+			if (companyLogo != null && CommonUtils.isPopulated(companyLogo.getOriginalFilename())) {
 				company.setCompanyLogo(companyLogo.getBytes());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			companyDetailsService.save(company);
-			if (company.getPageName() != null && !company.getPageName().trim().equals("")) {
-				modelAndView.setViewName("redirect:"+company.getPageName());
+				company.setCompanyStringLogo(CommonUtils.getImgfromByteArray(company.getCompanyLogo()));
 			} else {
-				modelAndView.addObject("message", "Company details added successfully!");
-				modelAndView.addObject("logoImage", CommonUtils.getImgfromByteArray(CompanyUploadedFile));
-				modelAndView.addObject("company", companyDetailsService.findByUserName(principal.getName()));
+				company.setCompanyLogo(company.getCompanyStringLogo().getBytes());
 			}
-		} else {
-			isDataExists.setCompanyAddress(company.getCompanyAddress());
-			isDataExists.setCompanyEmail(company.getCompanyEmail());
-			isDataExists.setCompanyGstin(company.getCompanyGstin());
-			isDataExists.setCompanyBankAC(company.getCompanyBankAC());
-			isDataExists.setCompanyBankIFSC(company.getCompanyBankIFSC());
-			isDataExists.setCompanyBankTNC(company.getCompanyBankTNC());
-			try {
-				CompanyUploadedFile = companyLogo.getBytes();
-				if (CompanyUploadedFile.length != 0) {
-					isDataExists.setCompanyLogo(companyLogo.getBytes());
-				} else {
-					CompanyUploadedFile = isDataExists.getCompanyLogo();
-					isDataExists.setCompanyLogo(isDataExists.getCompanyLogo());// Making changes if company page is
-																				// submitted w/o changing anything
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			isDataExists.setCompanyName(company.getCompanyName());
-			isDataExists.setCompanyState(company.getCompanyState());
-			isDataExists.setCompanyTelephone(company.getCompanyTelephone());
-			companyDetailsService.save(isDataExists);
-			modelAndView.addObject("logoImage", CommonUtils.getImgfromByteArray(CompanyUploadedFile));
-			modelAndView.addObject("message", "Company details updated successfully!");
-			modelAndView.addObject("company", companyDetailsService.findByUserName(principal.getName()));
+		} catch (IOException e) {
 		}
+
+		companyDetailsService.save(company);
+
+		modelAndView.addObject("logoImage", company.getCompanyStringLogo());
+		modelAndView.addObject("message", "Company details updated successfully!");
+		modelAndView.addObject("company", company);
+
 		return modelAndView;
 	}
 
