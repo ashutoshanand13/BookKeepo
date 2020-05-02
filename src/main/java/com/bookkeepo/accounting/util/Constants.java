3e1481f3d14844bed812442c6373fa90a82d0e09
Yash Singh <ys4002@gmail.com>
2020-05-02 16:45:13 +0530

    Bill of Supply

--- src/main/java/com/bookkeepo/accounting/util/Constants.java
+++ src/main/java/com/bookkeepo/accounting/util/Constants.java
@@ -19,6 +19,9 @@ public class Constants {
 	public static String CREDIT_DEBIT_NOTE_COLUMNS[] = { "Document type", "Document no.", "Document date",
 			"Original invoice no.", "Original invoice date", "Name of party", "GSTIN of party", "Taxable Value", "CGST",
 			"SGST", "IGST", "Total invoice amount" };
+	
+	public static String BILL_OF_SUPPLY[] = {"Document no.", "Document date",
+			"Date of supply", "Place of Supply", "Name of party", "Total invoice amount" };
 
 	public static String FORGET_PASSWORD_IDENTIFIER = "0";
 }
