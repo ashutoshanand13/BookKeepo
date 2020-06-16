06bf4dbf6cdce212248b7779d38825f82597140d
Yash Singh <ys4002@gmail.com>
2020-06-16 19:34:36 +0530

    payment and receipt delete done

--- src/main/java/com/bookkeepo/accounting/controller/ReceiptController.java
+++ src/main/java/com/bookkeepo/accounting/controller/ReceiptController.java
@@ -9,12 +9,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,8 +25,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.bookkeepo.accounting.entity.Accounts;
 import com.bookkeepo.accounting.entity.BankDetails;
 import com.bookkeepo.accounting.entity.Company;
-import com.bookkeepo.accounting.entity.Payment;
-import com.bookkeepo.accounting.entity.PaymentInvoices;
 import com.bookkeepo.accounting.entity.ReceiptInvoices;
 import com.bookkeepo.accounting.entity.Receipts;
 import com.bookkeepo.accounting.service.AccountService;
@@ -137,5 +137,26 @@ public class ReceiptController {
 		modelAndView.addObject("receiptList", receiptService.fetchAllReceipt(user));
 		modelAndView.setViewName("receiptData");
 		return modelAndView;
+	}
+	
+	@RequestMapping(value = { "/home/updatereceipt" }, method = RequestMethod.GET)
+	public ModelAndView updatePayment(Principal principal) {
+		ModelAndView modelAndView = new ModelAndView();
+		String user = principal.getName();
+		modelAndView.addObject("receiptList", receiptService.fetchAllReceipt(user));
+		modelAndView.setViewName("updateReceipt");
+		return modelAndView;
+	}
+
+	
+	@RequestMapping(value = { "/home/updatereceipt/{id}" })
+	public ModelAndView updatePaymentKey(@PathVariable("id") String id, HttpServletRequest request, HttpServletResponse response) {
+
+		String user = request.getUserPrincipal().getName();
+		receiptService.deleteUsersByIDAndUser(Integer.valueOf(id), user);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("redirect:/home/updatereceipt");
+		return modelAndView;
+		
 	}
 }
--- src/main/java/com/bookkeepo/accounting/repository/PaymentRepository.java
+++ src/main/java/com/bookkeepo/accounting/repository/PaymentRepository.java
@@ -6,6 +6,8 @@ package com.bookkeepo.accounting.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bookkeepo.accounting.entity.Company;
@@ -21,5 +23,9 @@ import com.bookkeepo.accounting.entity.Payment;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 	List<Payment> findByPaymentOwnerAndPaymentCompanyDetails(String owner, Company company);
+	
+	@Modifying
+    @Query("delete from Payment u where u.id = ?1 and u.paymentOwner = ?2")
+    void deleteUsersByIDAndUser(int id, String user);
 
 }
