b9991236455dc4ec7875c927c8d481640b7cd906
Ashu <shopashu@gmail.com>
2020-10-11 14:57:54 +0530

    Invoice Ledgers

--- src/main/java/com/bookkeepo/accounting/controller/LedgerInvoiceController.java
+++ src/main/java/com/bookkeepo/accounting/controller/LedgerInvoiceController.java
@@ -4,6 +4,9 @@
 package com.bookkeepo.accounting.controller;
 
 import java.security.Principal;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +20,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.bookkeepo.accounting.entity.Accounts;
 import com.bookkeepo.accounting.entity.Company;
+import com.bookkeepo.accounting.entity.InvoiceDetails;
+import com.bookkeepo.accounting.model.InvoiceType;
 import com.bookkeepo.accounting.model.LedgerColumns;
 import com.bookkeepo.accounting.model.LedgerInfo;
 import com.bookkeepo.accounting.model.LedgerInvoiceInfo;
@@ -41,32 +46,36 @@ public class LedgerInvoiceController extends MasterController {
 	}
 
 	@RequestMapping(value = { "/home/invoiceledger" }, method = RequestMethod.POST)
-	public ModelAndView generateLedger(@Valid LedgerInfo ledger, HttpServletRequest request, Principal principal) {
+	public ModelAndView generateLedger(@Valid LedgerInvoiceInfo ledger, HttpServletRequest request, Principal principal)
+			throws ParseException {
 		ModelAndView modelAndView = new ModelAndView();
 		String user = request.getUserPrincipal().getName();
 		Company company = companyDetailsService.findByUserName(user);
-		modelAndView.addObject("accountList",
-				accountService.fetchAccountName(request.getUserPrincipal().getName(), company));
 		if (CommonUtils.isPopulated(ledger.getStartDate()) && CommonUtils.isPopulated(ledger.getEndDate())
 				&& !CommonUtils.isValidEndDate(ledger.getStartDate(), ledger.getEndDate())) {
 			modelAndView.addObject("ledger", new LedgerInfo());
 			modelAndView.setViewName("ledger");
 			return modelAndView;
 		}
-
-		Map<Accounts, List<LedgerColumns>> ledgerMap = null;
-		Accounts account = accountService.findById(Integer.valueOf(ledger.getAccountId()));
-
-		if (account.getAccountType().equals(Constants.DEFAULT_ACCOUNT_ON_COMPANY_CREATION)
-				|| account.getAccountType().equals(Constants.DEFAULT_ACCOUNT_ON_BANK_CREATION)) {
-			ledgerMap = LedgerUtil.setUpCashAndBankLedgers(account, ledger, company);
+		Date startDate = CommonUtils.convertToDate(ledger.getStartDate());
+		Date endDate = CommonUtils.convertToDate(ledger.getEndDate());
+		endDate.setTime(endDate.getTime() + 86399000L);
+		List<String> invoiceTypeData = new ArrayList<String>();
+		if (!ledger.getInvoiceType().equals(InvoiceType.Purchase_Invoice.getType())) {
+			invoiceTypeData.add(InvoiceType.Tax_Invoice.getType());
+			invoiceTypeData.add(InvoiceType.Export_Invoice.getType());
+			invoiceTypeData.add(InvoiceType.Retail_Invoice.getType());
+			invoiceTypeData.add(InvoiceType.Bill_Supply.getType());
 		} else {
-			ledgerMap = LedgerUtil.setUpLedgers(accountService.findById(Integer.valueOf(ledger.getAccountId())),
-					ledger);
+			invoiceTypeData.add(ledger.getInvoiceType());
 		}
+		List<InvoiceDetails> invoiceData = invoiceService.findByStartEndDate(invoiceTypeData, user, startDate, endDate);
+
+		List<LedgerColumns> ledgerData = LedgerUtil.ledgerforInvoices(invoiceData);
+
 		modelAndView.addObject("ledger", ledger);
-		modelAndView.addObject("ledgerMap", ledgerMap);
-		modelAndView.setViewName("ledger");
+		modelAndView.addObject("ledgerData", ledgerData);
+		modelAndView.setViewName("invoiceledger");
 		return modelAndView;
 
 	}
